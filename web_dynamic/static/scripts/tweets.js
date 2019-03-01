4055bc9fc89a36267ae23044dbaf014e247842d8
robertglatzel <robertglatzel@gmail.com>
2019-03-01 07:11:51 +0000

    Adding updated styles and javascript. Js now returns a list of ids once page loads

--- web_dynamic/static/scripts/tweets.js
+++ web_dynamic/static/scripts/tweets.js
@@ -1,5 +1,36 @@
-$('.keep-tweet').click(function() {
-    $(this).parent().fadeOut(1000, function() {
-        $(this).parent().remove();
+$( document ).ready(function() {
+    let idList = [];
+    $('.tweet').each(function() {
+        let id = (this.id);
+        idList.push(id);
     });
+    if (idList.length === 0) {
+        $('body').css('background', '#ddd6f3');
+    }
+    console.log(idList);
+
+    // This function is for removing tweets from the list of all
+    // tweets that are to be deleted. The array containing all the Tweets
+    // will be updated.
+    $('.keep-tweet').click(function() {
+        $(this).parent().fadeOut(1500, function() {
+            let id = $(this).prop('id');
+            if (idList.includes(id)) {
+                let idIndex = idList.indexOf(id);
+                idList.splice(idIndex, 1);
+            }
+            $(this).parent().remove();
+            console.log(idList);
+        });
+    });
+    // This is the delete all flagged tweets function. It removes all remaining
+    // tweets.
+    $('#remove-all').click(function() {
+        $('#enclosure').fadeOut(2500, function() {
+            // Send the post request back to python with the remaining id's from
+            // this list.
+        });
+        $('body').css('background', '#ddd6f3');
+    })
+
 });
