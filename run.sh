15e3ad44ae9f96e43f6880d60d0657992e626418
VortexH <hemant.heer@gmail.com>
2019-03-09 23:34:51 +0000

    Added log files to gitignore
    Redirects pip3 install and virtualenv set up stdout to txt files

--- run.sh
+++ run.sh
@@ -3,11 +3,11 @@ export FLASK_APP=detweet_app
 export FLASK_ENV=development
 if [ ! -d detweet_env ]; then
     echo "Virtual environment does not exist..creating one now"
-    virtualenv detweet_env
+    virtualenv detweet_env 1>detweet_creation_log.txt
     echo "Activating virtual environment.."
     source detweet_env/bin/activate
     echo "Installing dependencies required by app...hold on!"
-    pip3 install -r requirements.txt
+    pip3 install -r requirements.txt 1>install_log.txt
     echo "Installed all dependencies in virtual environment.."
 else
     source detweet_env/bin/activate
