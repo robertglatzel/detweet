2422de1be5b7da1ae77dd83a169eb6d523905f12
VortexH <hemant.heer@gmail.com>
2019-03-10 00:11:13 +0000

    /get_tweets endpoint now returns a list of tweets as JSON

--- detweet_app/views.py
+++ detweet_app/views.py
@@ -17,24 +17,33 @@ def index():
     resp = twitter.get("account/verify_credentials.json")
     assert resp.ok
     
-
     screen_name = resp.json()['screen_name']
     print(screen_name)
-    return redirect(url_for('tweet_page', username=screen_name))
+    return redirect(url_for('tweet_page', username=screen_name,
+        tweets=None))
 
-@app.route('/tweet_page/<username>')
-def tweet_page(username):
-    return render_template('index.html', username=username)
+@app.route('/tweet_page/<username>/<tweets>')
+def tweet_page(username, tweets=None):
+    return render_template('index.html', username=username, tweets=tweets)
 
 @app.route('/get_tweets')
 def get_tweets():
+    """
     resp = twitter.get("account/verify_credentials.json")
     assert resp.ok
     payload = {'screen_name': resp.json()['screen_name']}
     resp = twitter.get('statuses/user_timeline.json', params=payload)
     print(type(resp.json()))
-    return render_template('index.html', tweets=deTweet.safety(resp.json()))
-    #return jsonify(resp.json())
+    #return render_template('index.html', tweets=deTweet.safety(resp.json()))
+    return redirect(url_for('tweet_page', username=resp.json()['screen_name'], tweets=deTweet.safety(resp.json())))
+    """
+
+    resp = twitter.get("account/verify_credentials.json")
+    assert resp.ok
+    payload = {'screen_name': resp.json()['screen_name']}
+    resp = twitter.get('statuses/user_timeline.json', params=payload)
+    tweets = deTweet.safety(resp.json())
+    return jsonify(tweets)
 
 if __name__ == '__main__':
     app.run('0.0.0.0', 5000)
