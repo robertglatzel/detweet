3724ff5797a95e1d8f8e4e844f2edaa46ba03ac1
VortexH <hemant.heer@gmail.com>
2019-03-13 00:41:00 +0000

    removed colon from endpoint string because we were getting error 34 responses from the twitter api

--- detweet_app/deTweet.py
+++ detweet_app/deTweet.py
@@ -67,7 +67,7 @@ def delete_tweets(twitter_req_obj, request):
             try:
                 tweet_id = int(tweet_id)
                 print(tweet_id)
-                endpoint_build = "statuses/destroy/:{}.json".format(tweet_id)
+                endpoint_build = "statuses/destroy/{}.json".format(tweet_id)
                 print(endpoint_build)
                 resp = twitter_req_obj.post(endpoint_build).json()
                 print(resp)
