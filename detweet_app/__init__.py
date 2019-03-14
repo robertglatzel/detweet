63733c586d778c88122496c792194a45632eb4e2
VortexH <hemant.heer@gmail.com>
2019-03-14 20:25:40 +0000

    Experimenting with flask_login

--- detweet_app/__init__.py
+++ detweet_app/__init__.py
@@ -3,13 +3,17 @@ from flask import Flask
 from flask_dance.consumer.storage.sqla import OAuthConsumerMixin, SQLAlchemyStorage
 from flask_dance.contrib.twitter import make_twitter_blueprint
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import (
+        LoginManager, UserMixin, current_user,
+        login_required, login_user, logout_user
+)
 
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_pyfile('config.py')
 app.url_map.strict_slashes = False
 
 db = SQLAlchemy(app)
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(250), unique = True)
 
@@ -19,8 +23,11 @@ class OAuth(OAuthConsumerMixin, db.Model):
 db.create_all()
 
 twitter_bp = make_twitter_blueprint(redirect_to='index')
-twitter_bp.storage = SQLAlchemyStorage(OAuth, db.session)
+twitter_bp.storage = SQLAlchemyStorage(OAuth, db.session, user=current_user)
 
 app.register_blueprint(twitter_bp, url_prefix='/login')
+
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 import detweet_app.views
--- detweet_app/views.py
+++ detweet_app/views.py
@@ -1,14 +1,63 @@
 #!/usr/bin/python3
 """ Script to start a Flask web application """
 
-from detweet_app import app, twitter_bp
+from detweet_app import app, twitter_bp, login_manager, login_user, logout_user, db
 from flask import jsonify, redirect, render_template, request, url_for, session
 from flask_cors import CORS
+from flask_dance.consumer import oauth_authorized
 from flask_dance.contrib.twitter import twitter
 from .deTweet import get_all_tweets, delete_tweets
 import re
 
 CORS(app, resources={r"*": {"origins": "*"}})
+
+#create/Login Local user on Succesful OAuth Login
+@oauth_authorized.connect_via(blueprint)
+def hubspot_logged_in(blueprint, token):
+  if not token:
+      flash("Failed to log in with twitter", category="error")
+      return False
+
+  resp = blueprint.session.get("account/verify_credentials.json")
+  if not resp.ok:
+      msg = "Failed to fetch user info from Twitter"
+      flash(msg, category="error")
+      return False
+
+  twitter_info = resp.json()
+  twitter_user_id = str(twitter_info['id'])
+
+  # Find this OAuth token in the database, or create it
+  query = OAuth.query.filter_by(
+    provider=blueprint.name,
+    provider_user_id=twitter_user_id)
+  )
+  try:
+    oauth = query.one()
+  except NoResultFound:
+    oauth = Oauth(
+      provider=blueprint.name,
+      provider_user_id=hubspot_user_id,
+      token=token,
+    )
+
+  if oauth.user:
+    login_user(oauth.user)
+    flash("Successfully signed in with Twitter")
+  else:
+    # Create a new local user account for this user
+    user = User(
+      # Remember that 'email' can be None, if the user declines
+      username=twitter_info["screen_name"]
+      id = twitter_info['id']
+    )
+    oauth.user = user
+    db.session.add_all([user, oauth])
+    db.session.commit()
+    login_user(user)
+    flash("Successfully signed in with hubspot")
+
+    return False
 
 @app.route('/')
 def serve_login_page():
