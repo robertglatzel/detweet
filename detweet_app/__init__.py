d40736f39570d5827c4cb5a0aa6690bc00518bbc
VortexH <hemant.heer@gmail.com>
2019-03-14 00:48:01 +0000

    Created classes for user authentication and tokens.
    twitter_bp now contains a redirect_to parameter that redirects the page
    to the index view instead of the default / view.

--- detweet_app/__init__.py
+++ detweet_app/__init__.py
@@ -9,11 +9,16 @@ app.config.from_pyfile('config.py')
 app.url_map.strict_slashes = False
 
 db = SQLAlchemy(app)
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(250), unique = True)
+
 class OAuth(OAuthConsumerMixin, db.Model):
-    pass
+    user_id = db.Column(db.Integer, db.ForeignKey(User.id))
+    user = db.relationship(User)
 db.create_all()
 
-twitter_bp = make_twitter_blueprint()
+twitter_bp = make_twitter_blueprint(redirect_to='index')
 twitter_bp.storage = SQLAlchemyStorage(OAuth, db.session)
 
 app.register_blueprint(twitter_bp, url_prefix='/login')
