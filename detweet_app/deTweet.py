911ebedcd9fe31dac359256b96d5e2f53614622c
robertglatzel <robertglatzel@gmail.com>
2019-03-19 01:51:35 +0000

    Trying to improve regex, bolding only works on some of the words

--- detweet_app/deTweet.py
+++ detweet_app/deTweet.py
@@ -55,16 +55,17 @@ def filter_tweets(tweets, user_filter=None):
 
     try:
         for tweet in tweets:
+            tweet_text_lower = re.findall(r"[\w']+", tweet['full_text'].lower())
+            #                tweet_text_orig = re.findall(r"[\w']+", tweet['full_text'])
+            #                tweet_text_lower = tweet['full_text'].lower().split()
+            tweet_text_orig = tweet['full_text']
             for word in bad_words:
-                tweet_text_lower = re.findall(r"[\w']+", tweet['full_text'].lower())
-                tweet_text_orig = re.findall(r"[\w']+", tweet['full_text'])
-#                tweet_text_lower = tweet['full_text'].lower().split()
-#                tweet_text_orig = tweet['full_text'].split()
                 if word in tweet_text_lower:
-                    idx = tweet_text_lower.index(word)
-                    strong_word = '<strong>{}</strong>'.format(tweet_text_orig[idx])
-                    tweet_text_orig[idx] = strong_word
-                    tweet_dict = {tweet.get('id'): '"{}"'.format(' '.join(tweet_text_orig))}
+                    tweet_text_orig = tweet_text_orig.replace(word, '<strong>{}</strong>'.format(word))
+#                    idx = tweet_text_lower.index(word)
+#                    strong_word = '<strong>{}</strong>'.format(tweet_text_orig[idx])
+#                    tweet_text_orig[idx] = strong_word
+                    tweet_dict = {tweet.get('id'): '"{}"'.format(tweet_text_orig)}
                     bad_tweet_list.append(tweet_dict)
     except Exception as e:
         print(e)
