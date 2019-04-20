f40b25ea757b8a8d297d9c358e12f4b8b0f9483e
VortexH <hemant.heer@gmail.com>
2019-04-20 04:35:34 +0000

    installs libmysqlclient-dev to prevent mysql_config not found error and deletes the export and run lines because the script's purpose is to install the basic software need to install the application

--- install_script.sh
+++ install_script.sh
@@ -1,22 +1,8 @@
 #!/usr/bin/env bash
 # Sets up the virtual environment with all the dependencies listed in the requirements.txt file and runs the flask app on 0.0.0.0
-export FLASK_APP=detweet_app
-export FLASK_ENV=development
 
 sudo apt-get update
 sudo apt-get -y install python3-pip
 sudo apt-get -y install mysql-server
 echo 'CREATE DATABASE IF NOT EXISTS detweet' | mysql -uroot -p
-
-if [ ! -d detweet_env ]; then
-    echo "Virtual environment does not exist..creating one now"
-    virtualenv detweet_env 1>detweet_creation_log.txt
-    echo "Activating virtual environment.."
-    source detweet_env/bin/activate
-    echo "Installing dependencies required by app...hold on!"
-    pip3 install -r requirements.txt 1>install_log.txt
-    echo "Installed all dependencies in virtual environment.."
-else
-    source detweet_env/bin/activate
-fi
-flask run --host=0.0.0.0
+sudo apt-get -y install libmysqlclient-dev
