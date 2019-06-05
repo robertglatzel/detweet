99f56d82e96f2188cd0839cb1e0c1be336193124
VortexH <hemant.heer@gmail.com>
2019-06-05 15:56:29 -0400

    major rewrite of authentication using Auth0

--- detweet_app/__init__.py
+++ detweet_app/__init__.py
@@ -1,15 +1,24 @@
 #!/usr/bin/env python3
 
 from flask import Flask
-from .models import db, login_manager
-from .oauth import blueprint
-from .cli import create_db
+from authlib.flask.client import OAuth
 
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_pyfile('config.py')
 app.url_map.strict_slashes = False
-app.register_blueprint(blueprint, url_prefix="/login")
-app.cli.add_command(create_db)
-db.init_app(app)
-login_manager.init_app(app)
+
+oauth = OAuth(app)
+
+auth0 = oauth.register(
+    'auth0',
+    client_id='G66gVi4HPmHHhJVRXD5alEZzlU02PuG2',
+    client_secret = 'GlKWByC67S0vTv_9ZSMqcrZnC_LAeccN8ghUvQYxxobOjuKaMTVL9bkrwjDoIftV',
+    api_base_url = 'https://dev-p8knhxpg.auth0.com',
+    access_token_url = 'https://dev-p8knhxpg.auth0.com/oauth/token',
+    authorize_url = 'https://dev-p8knhxpg.auth0.com/authorize',
+    client_kwargs = {
+        'scope': 'openid profile'
+    }
+)
+
 import detweet_app.views
--- detweet_app/views.py
+++ detweet_app/views.py
@@ -1,39 +1,77 @@
 #!/usr/bin/python3
-""" Script to start a Flask web application """
 
-from detweet_app import app
-from .oauth import blueprint
-from .models import OAuth, User
-from flask import jsonify, redirect, render_template, request, url_for, session
+from detweet_app import app, auth0
+from flask import jsonify, redirect, render_template, url_for, session
 from flask_cors import CORS
-from flask_dance.contrib.twitter import twitter
-from flask_login import login_required, logout_user, current_user
 from .deTweet import get_all_tweets, delete_tweets
 import re
 from uuid import uuid4
+from functools import wraps
+import json
+from os import environ as env
+from werkzeug.exceptions import HTTPException
 
 
 CORS(app, resources={r"*": {"origins": "*"}})
+
+def requires_auth(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        if 'profile' not in session:
+            return redirect('/')
+        return f(*args, **kwargs)
+
+    return decorated
 
 @app.route('/')
 def serve_login_page():
     return render_template('login.html')
 
+@app.route('/callback')
+def callback_handling():
+    # Handles response from token endpoint
+
+    auth0.authorize_access_token()
+    resp = auth0.get('user_info')
+    user_info = resp.json()
+
+    session['jwt_payload'] = user_info
+    session['profile'] = {
+        'user_id': user_info['sub'],
+        'name': user_info['name'],
+        'picture': user_info['picture']
+    }
+
+    return redirect('/tweet_page')
+
+
 @app.route('/login')
-def index():
-    return redirect(url_for('twitter.login'))
+def login():
+    headers = {
+        'response_type': 'token',
+        'connection': 'twitter',
+        'redirect_uri': 'http://localhost:5000/tweet_page'
+    }
+    auth0.get('/authorize', headers=headers)
 
 @app.route('/tweet_page')
-@login_required
+@requires_auth
 def tweet_page():
-    img = current_user.image_url
+
+    return render_template('index.html')
+
+    """
+    current_user = session['profile']
+    img = current_user.get('picture')
     img_no_normal = ''.join(re.split("_normal", img))
     return render_template(
             'index.html',
-            username = current_user.name,
+            username = current_user.get('name'),
             info = current_user.description,
             img = img_no_normal
             )
+
+    """
 
 
 @app.route('/get_tweets', methods=['POST'])
@@ -53,18 +91,14 @@ def tweet_deleter():
     return jsonify(ret_status)
 
 @app.route('/logout')
-@login_required
 def session_logout():
-    """ Deletes the OAuth token from the database and redirects the user
-        to the serve_login_page view
-    """
-    logout_user()
-    return redirect(url_for('serve_login_page'))
+    session.clear()
+
+    params = {
+        'returnTo': url_for('')
+    }
 
 
 @app.errorhandler(404)
 def handle_error(error):
     return render_template('404page.html'), 404
-
-if __name__ == '__main__':
-    app.run('0.0.0.0', 5000)
