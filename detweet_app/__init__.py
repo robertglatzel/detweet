38c1be85446e1134e0a88654fd5aa1fae5c8e16f
VortexH <hemant.heer@gmail.com>
2019-06-12 00:04:52 -0400

    conversion to session storage so people can start using the application

--- detweet_app/__init__.py
+++ detweet_app/__init__.py
@@ -1,15 +1,12 @@
 #!/usr/bin/env python3
 
 from flask import Flask
-from .models import db, login_manager
-from .oauth import blueprint
-from .cli import create_db
+from flask_dance.contrib.twitter import make_twitter_blueprint
 
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_pyfile('config.py')
 app.url_map.strict_slashes = False
+
+blueprint = make_twitter_blueprint(redirect_to='tweet_page')
 app.register_blueprint(blueprint, url_prefix="/login")
-app.cli.add_command(create_db)
-db.init_app(app)
-login_manager.init_app(app)
 import detweet_app.views
--- detweet_app/views.py
+++ detweet_app/views.py
@@ -1,19 +1,27 @@
 #!/usr/bin/python3
 """ Script to start a Flask web application """
 
-from detweet_app import app
-from .oauth import blueprint
-from .models import OAuth, User
+from detweet_app import app, blueprint
 from flask import jsonify, redirect, render_template, request, url_for, session
 from flask_cors import CORS
 from flask_dance.contrib.twitter import twitter
-from flask_login import login_required, logout_user, current_user
 from .deTweet import get_all_tweets, delete_tweets
 import re
 from uuid import uuid4
 
 
 CORS(app, resources={r"*": {"origins": "*"}})
+
+
+def getUserInformation():
+    resp = twitter.get('account/verify_credentials.json')
+
+    info = resp.json()
+
+    keys_to_store = ['screen_name', 'profile_image_url_https','description']
+
+    for item in keys_to_store:
+        session[item] = info[item]
 
 @app.route('/')
 def serve_login_page():
@@ -24,17 +32,16 @@ def index():
     return redirect(url_for('twitter.login'))
 
 @app.route('/tweet_page')
-@login_required
 def tweet_page():
-    img = current_user.image_url
+    getUserInformation()
+    img = session['profile_image_url_https']
     img_no_normal = ''.join(re.split("_normal", img))
     return render_template(
             'index.html',
-            username = current_user.name,
-            info = current_user.description,
+            username = session['screen_name'],
+            info = session['description'],
             img = img_no_normal
             )
-
 
 @app.route('/get_tweets', methods=['POST'])
 def get_tweets():
@@ -53,12 +60,12 @@ def tweet_deleter():
     return jsonify(ret_status)
 
 @app.route('/logout')
-@login_required
 def session_logout():
     """ Deletes the OAuth token from the database and redirects the user
         to the serve_login_page view
     """
-    logout_user()
+    for key in session.keys():
+        session.pop(key)
     return redirect(url_for('serve_login_page'))
 
 
