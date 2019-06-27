8cfb0258661ec272591e0edd108eaf469d3b6afe
Robert Glatzel <robertglatzel@gmail.com>
2019-06-26 21:10:54 -0700

    Changed Tweet.js to stateless component

--- detweet-react/src/components/Tweet.js
+++ detweet-react/src/components/Tweet.js
@@ -1,29 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Tweet extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {};
-	}
-	render() {
-		return (
-			<div className="column centered">
-				<div id={this.props.id} className="tweet ui segment hvr-float-shadow">
-					<div className="top">
-						<p>You tweeted:</p>
-					</div>
-					<button
-						onClick={this.props.keep}
-						className="keep-tweet ui button violet hvr-pulse-grow"
-						title="Keep me!"
-					>
-						Keep me
-					</button>
-					<p className="tweet-text">{this.props.text}</p>
+const Tweet = (props) => {
+	return (
+		<div className="column centered">
+			<div id={props.id} className="tweet ui segment hvr-float-shadow">
+				<div className="top">
+					<p>You tweeted:</p>
 				</div>
+				<button onClick={props.keep} className="keep-tweet ui button violet hvr-pulse-grow" title="Keep me!">
+					Keep me
+				</button>
+				<p className="tweet-text">{props.text}</p>
 			</div>
-		);
-	}
-}
+		</div>
+	);
+};
 
 export default Tweet;
