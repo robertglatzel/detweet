8fc85b158f471f90a601011f2de0b36002df7a03
Robert Glatzel <robertglatzel@gmail.com>
2019-06-26 21:07:32 -0700

    changes Login to stateless component

--- detweet-react/src/components/Login.js
+++ detweet-react/src/components/Login.js
@@ -1,31 +1,29 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Logo from './Logo';
 
-class Login extends Component {
-	render() {
-		return (
-			<div>
-				<Logo />
-				<div id="sign-in" className="instructions">
-					<p className="ui center aligned">
-						Welcome to <strong>deTweet</strong>, a Twitter application that cleans your profile of unwanted
-						tweets by filtering your tweets against a database of flagged words. We make it easy for you to
-						review and remove problematic tweets with the click of a button. <br /> <br />
-						Sign in below to get started, or login with a dummy profile to check out deTweet's features!
-					</p>
-					<button id="login-button" className="ui basic button centered" onClick={this.props.login}>
-						<i className="twitter icon" />
-						<p>Sign in with Twitter</p>
-					</button>
-					<br />
-					<button id="dummy-button" className="ui basic button centered" onClick={this.props.dummy}>
-						<i className="twitter icon" />
-						<p>Dummy Profile</p>
-					</button>
-				</div>
+const Login = (props) => {
+	return (
+		<div>
+			<Logo />
+			<div id="sign-in" className="instructions">
+				<p className="ui center aligned">
+					Welcome to <strong>deTweet</strong>, a Twitter application that cleans your profile of unwanted
+					tweets by filtering your tweets against a database of flagged words. We make it easy for you to
+					review and remove problematic tweets with the click of a button. <br /> <br />
+					Sign in below to get started, or login with a dummy profile to check out deTweet's features!
+				</p>
+				<button id="login-button" className="ui basic button centered" onClick={props.login}>
+					<i className="twitter icon" />
+					<p>Sign in with Twitter</p>
+				</button>
+				<br />
+				<button id="dummy-button" className="ui basic button centered" onClick={props.dummy}>
+					<i className="twitter icon" />
+					<p>Dummy Profile</p>
+				</button>
 			</div>
-		);
-	}
-}
+		</div>
+	);
+};
 
 export default Login;
