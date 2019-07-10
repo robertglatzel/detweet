550787241264509d6224089fbc4fbf482c17d6f4
VortexH <hemant.heer@gmail.com>
2019-07-10 19:00:26 -0400

    Fixed an issue where global_tweet_list contained multiple sets of the same tweets. Now includes error checking when traversing the authenticated user's timeline

--- detweet_app/functions/get_all_tweets.py
+++ detweet_app/functions/get_all_tweets.py
@@ -1,23 +1,31 @@
-def get_all_tweets(twitter_req_obj):
-    '''
-    Grabs 3200 the tweets from the twitter api in their full text form.
+def get_all_tweets(twitter):
+    ''' Tries to grab 3200 tweets from a user's timeline
+
+        Args:
+            twitter:
+                object containing tokens to make requests
+                against the Twitter API
+
+        Returns:
+            list: list of tweet objects from a user's timeline
     '''
     global_tweet_list = []
     last_tweet_id = None
     payload = {
         'count': 200,
+        'tweet_mode': 'extended',
         'include_rts': 1,
-        'tweet_mode': 'extended'
     }
     for i in range(16):
-        if (last_tweet_id) is not None:
-            payload = {
-                'max_id': last_tweet_id,
-            }
-        tweet_list = twitter_req_obj.get(
+        if last_tweet_id is not None:
+            payload['max_id'] = last_tweet_id - 1
+        tweet_list = twitter.get(
             'statuses/user_timeline.json',
             params=payload
             ).json()
+        if len(tweet_list) == 0:
+            print('All tweets have been retrieved')
+            break
         last_tweet_id = tweet_list[-1].get('id')
         global_tweet_list += tweet_list
 
