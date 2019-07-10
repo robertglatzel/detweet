e8a49a88b7f853161a4cca1a8660104643244572
VortexH <hemant.heer@gmail.com>
2019-07-10 19:53:45 -0400

    Regular expression now uses the word from user_filter as the expression instead of parsing the text, parsing non word characters, and piecing the tweet text. Added descriptive docstring to function.

--- detweet_app/functions/filter_tweets.py
+++ detweet_app/functions/filter_tweets.py
@@ -2,39 +2,47 @@ from os.path import abspath
 import re
 
 
-def filter_tweets(tweets, user_filter=None):
+def filter_tweets(tweets, user_filter):
+    ''' Iterates through all tweets and marks the tweet based on a bunch of
+        terms that the user has entered or our own file of bad words
+
+    Args:
+        tweets (list): list of tweet objects obtained from the Twitter API
+        user_filter (list): list of words to flag within the tweet text
+                            If no terms were entered by the user, this filter
+                            becomes a list of bad words from a file stored in
+                            the repo. Otherwise, this filter will use the terms
+                            entered by the user.
+
+    Returns:
+        list: bad_tweet_list. can be empty (no flagged tweets) or contain
+        a list of dicts e.g. [{tweet_id: flagged_tweet}, ... ]
+    '''
     bad_tweet_list = []
 
+    # Handles the case where the user_filter is empty. It still comes in as
+    # a list that contains something
     if user_filter[0] == '':
         f_path = abspath("bad_words_list_less")
         with open(f_path) as f:
             bad_words = [word.rstrip('\n') for word in f]
     else:
-        user_bad_word = user_filter
-        bad_words = [word.lower() for word in user_bad_word]
+        # Case where the user entered a bunch of terms in the box
+        bad_words = [word.lower() for word in user_filter]
 
-    try:
-        for tweet in tweets:
-            for word in bad_words:
-                txt_low = re.findall(r"[^#!.\s]+", tweet['full_text'].lower())
-                txt_orig = re.findall(r"[^#!.\s]+", tweet['full_text'])
-                txt_space = re.findall(r"[#!.\s]+", tweet['full_text'])
-                if word in txt_low:
-                    idx = txt_low.index(word)
-                    strong_word = '<strong>{}</strong>'.format(txt_orig[idx])
-                    txt_orig[idx] = strong_word
-                    lst_rebld = []
-                    for i in range(len(txt_orig)):
-                        lst_rebld.append(txt_orig[i])
-                        try:
-                            lst_rebld.append(txt_space[i])
-                        except:
-                            pass
-                    tweet_dict = {
-                        tweet.get('id'): '"{}"'.format(''.join(lst_rebld))
-                        }
-                    bad_tweet_list.append(tweet_dict)
-    except Exception as e:
-        print(e)
+    for tweet in tweets:
+        for word in bad_words:
+            # Naive solution to force regex
+            # to interpret * and + characters literally
+            word = word.replace('+', '\+')
+            word = word.replace('*', '\*')
+            regex = r'{}'.format(word)
+            repl = '<strong>{}</strong>'.format(word)
+            marked_tweet = re.sub(regex, repl, tweet['full_text'])
+            if marked_tweet != tweet['full_text']:
+                tweet_dict = {
+                    tweet.get('id'): '"{}"'.format(marked_tweet)
+                    }
+                bad_tweet_list.append(tweet_dict)
 
     return bad_tweet_list
