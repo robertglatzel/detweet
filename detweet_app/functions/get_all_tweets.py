cf386bfe3f5c7b5d83150928cc4c5f85edca4796
VortexH <hemant.heer@gmail.com>
2019-07-10 20:31:38 -0400

    Commented out code that involves dumping the global tweet list to a json file to verify that all tweets are obtained from a user's timeline

--- detweet_app/functions/get_all_tweets.py
+++ detweet_app/functions/get_all_tweets.py
@@ -1,3 +1,5 @@
+#from json import dumps
+
 def get_all_tweets(twitter):
     ''' Tries to grab 3200 tweets from a user's timeline
 
@@ -28,5 +30,19 @@ def get_all_tweets(twitter):
             break
         last_tweet_id = tweet_list[-1].get('id')
         global_tweet_list += tweet_list
+
+    '''
+    with open('hemant_tweet_timeline.json', mode='w') as file_:
+        file_.write(dumps(global_tweet_list))
+    '''
+
+    '''
+    test = []
+    for tweet_obj in global_tweet_list:
+        test.append(tweet_obj.get('full_text'))
+
+    if len(set(test)) == len(test):
+        print("All unique")
+    '''
 
     return global_tweet_list
